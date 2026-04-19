3371ed6e0e8949868206b4ab7d8beac49a549837
Thayná Vieira dos Santos <thayna.vieira@academico.ifpb.edu.br>
2026-04-19 16:13:24 -0300

    Teste correto

--- src/app.py
+++ src/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+API_KEY = "123456"
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 @app.route("/")
 def home():
-    return "FUNCIONANDO NOVO"
+    return "FUNCIONANDO"
 
 if __name__ == "__main__":
     app.run(port=3000)
